cd481edb13498fa4e329140e1403b71c92d9096e
jdboachie <judeboachiekwame@gmail.com>
2023-05-17 16:03:09 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Module that defines the BaseModel class."""
 import uuid
 from datetime import datetime
 
@@ -7,13 +9,25 @@ class BaseModel:
     Base class for other models.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list (not used).
+            **kwargs: Arbitrary keyword arguments.
+                Each key in kwargs represents an attribute name, and the corresponding value is the attribute value.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
 
     def __str__(self):
         """
@@ -22,9 +36,7 @@ class BaseModel:
         Returns:
             str: A string representing the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id,
-                                     self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -37,8 +49,7 @@ class BaseModel:
         Converts the BaseModel instance to a dictionary.
 
         Returns:
-            dict: A dictionary containing all the attributes
-                  of the BaseModel instance.
+            dict: A dictionary containing all the attributes of the BaseModel instance.
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
