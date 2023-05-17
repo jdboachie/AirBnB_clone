49ddd4967e7d81477cc9a133843595b91241970f
jdboachie <judeboachiekwame@gmail.com>
2023-05-17 15:49:30 +0000

    Update test_base_model.py

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,42 +1,58 @@
-import datetime
 import unittest
-from uuid import uuid4
-
 from models.base_model import BaseModel
+from datetime import datetime
+import json
 
 
 class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel()
 
-    def test_init(self):
-        """Test the __init__ method"""
-        model = BaseModel()
-        # self.assertAlmostEquals(model.id, uuid4())
-        self.assertIsInstance(datetime.datetime.strptime(model.created_at,
-                                                         "%Y-%m-%dT%H:%M:%S.%f"),
-                              datetime.datetime)
-        self.assertIsInstance(datetime.datetime.strptime(model.updated_at,
-                                                         "%Y-%m-%dT%H:%M:%S.%f"),
-                              datetime.datetime)
+    def test_attributes_existence(self):
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
 
-    def test_str(self):
-        """Test the __str__ method"""
-        model = BaseModel()
-        expected_str = f"[{model.__class__.__name__}] ({model.id}) <{model.__dict__}>"
-        self.assertEqual(str(model), expected_str)
+    def test_id_is_string(self):
+        self.assertIsInstance(self.model.id, str)
 
-    def test_save(self):
-        """Test the save method"""
-        model = BaseModel()
-        model.save()
-        self.assertGreaterEqual(model.updated_at, model.created_at)
+    def test_created_at_is_datetime(self):
+        self.assertIsInstance(self.model.created_at, datetime)
 
-    def test_to_dict(self):
-        """Test the to_dict method"""
-        model = BaseModel()
-        expected_dict = model.__dict__.copy()
-        expected_dict["__class__"] = model.__class__.__name__
-        self.assertEqual(model.to_dict(), expected_dict)
+    def test_updated_at_is_datetime(self):
+        self.assertIsInstance(self.model.updated_at, datetime)
+
+    def test_str_representation(self):
+        self.assertEqual(str(self.model), "[BaseModel] ({}) {}".format(self.model.id, self.model.__dict__))
+
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertGreaterEqual(old_updated_at, self.model.updated_at)
+
+    def test_to_dict_returns_dictionary(self):
+        dictionary = self.model.to_dict()
+        self.assertIsInstance(dictionary, dict)
+
+    def test_to_dict_contains_all_attributes(self):
+        dictionary = self.model.to_dict()
+        self.assertEqual(dictionary['id'], self.model.id)
+        self.assertEqual(dictionary['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(dictionary['updated_at'], self.model.updated_at.isoformat())
+
+    def test_to_dict_contains_class_name(self):
+        dictionary = self.model.to_dict()
+        self.assertEqual(dictionary['__class__'], 'BaseModel')
+
+    def test_to_dict_only_contains_instance_attributes(self):
+        dictionary = self.model.to_dict()
+        self.assertEqual(len(dictionary.keys()), len(self.model.__dict__) + 1)  # +1 for '__class__'
+
+    def test_to_dict_json_serializable(self):
+        dictionary = self.model.to_dict()
+        json_string = json.dumps(dictionary)
+        self.assertIsInstance(json_string, str)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
