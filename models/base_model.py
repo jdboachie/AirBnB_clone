3e3153384865ee5415973c38f3357d08e35669ea
jdboachie <judeboachiekwame@gmail.com>
2023-06-08 00:13:43 +0000

    feat: update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -50,8 +50,6 @@ class BaseModel:
         """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.strftime(
-            "%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict["updated_at"] = self.created_at.strftime(
-            "%Y-%m-%dT%H:%M:%S.%f")
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.created_at.isoformat()
         return obj_dict
