f4c87ce2d2353d9939a45c4325069c641edb3bc5
jdboachie <judeboachiekwame@gmail.com>
2023-06-08 09:45:09 +0000

    feat: Update console.py

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ import json
 from shlex import split
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
