b623f84201d1626bd50cd6c2606b857d9bce1d55
jdboachie <judeboachiekwame@gmail.com>
2023-06-08 09:14:19 +0000

    feat: create new classes

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines the Amenity class.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents an Amenity for a MySQL database."""
+    name = ""
